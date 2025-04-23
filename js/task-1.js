6741115f77ec67158b01d48ab8333bef6fa9e58a
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-04-23 17:05:40 +0300

    refactor code with  keyword

--- js/task-1.js
+++ js/task-1.js
@@ -5,20 +5,20 @@ const customer = {
     orders: ["Burger", "Pizza", "Salad"],
     // Change code below this line
     getBalance() {
-        return balance;
+        return this.balance;
     },
     getDiscount() {
-        return discount;
+        return this.discount;
     },
     setDiscount(value) {
-        discount = value;
+        this.discount = value;
     },
     getOrders() {
-        return orders;
+        return this.orders;
     },
     addOrder(cost, order) {
-        balance -= cost - cost * discount;
-        orders.push(order);
+        this.balance -= cost - cost * this.discount;
+        this.orders.push(order);
     },
     // Change code above this line
 };
